255421443aa56dddce87978cfacb481f97d03195
Emrah Billur <emrah.billur@unikie.com>
2024-06-04 12:40:51 +0300

    Orin X11 with Java
    Signed-off-by: Emrah Billur <emrah.billur@unikie.com>
    
    libGL.so.1 not found fix
    
    Signed-off-by: Emrah Billur <emrah.billur@unikie.com>

---
+++ modules/common/development/java.nix
@@ -0,0 +1,25 @@
+# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.ghaf.development.java;
+in
+  with lib; {
+    options.ghaf.development.java = {
+      enable = mkEnableOption "Java Support";
+    };
+
+    config = mkIf cfg.enable {
+      programs.java = {
+        enable = true;
+        package = pkgs.jdk20;
+      };
+      environment.variables = {
+        LD_LIBRARY_PATH = pkgs.libGL.outPath+"/lib";
+      };
+    };
+  }
